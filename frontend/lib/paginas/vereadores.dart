a9de8edb2c024ad83c79a36968a4e38c69e1b0b6
Sophia Mileski <sophiamileskisantos@gmail.com>
2026-04-28 14:57:08 -0300

    criação da telavereadores

---
+++ frontend/lib/paginas/vereadores.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: TelaVereadores(),
+  ));
+}
+
+class TelaVereadores extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+    )
+    
+    
+  }
+}
