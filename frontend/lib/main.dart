1fff83642d8fe2657a0d2d281a20e5efbf7c20b8
schwankepaola <paolaschwanke@gmail.com>
2026-05-05 14:24:19 -0300

    comit

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,12 +1,7 @@
 import 'package:flutter/material.dart';
 import 'paginas/vereadores.dart';
-<<<<<<< HEAD
-import 'paginas/vereador_individual.dart';
-
-=======
 import 'paginas/partido.dart';
 import 'paginas/partido_individual.dart';
->>>>>>> 9df9e303c99482d9a8224e415431c69c93477abe
 void main() {
   runApp(MyApp());
 }
