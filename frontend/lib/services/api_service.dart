c4b7816b5f2c9fb7f67bf935e35d53f926154c1e
Bruna da Silva Donati <brunadasilvadonati@gmail.com>
2026-06-16 15:22:24 -0300

    mudança api services

--- frontend/lib/services/api_service.dart
+++ frontend/lib/services/api_service.dart
@@ -1,15 +1,40 @@
-import "../mock/mock_data.dart";
+import 'dart:convert';
+import 'package:http/http.dart' as http;
 
 class ApiService {
+  static const String baseUrl =
+      "http://10.0.2.2:5001/transparencia-municipal-4e915/us-central1";
+
   Future<List<dynamic>> getProjetos() async {
-    return projetosMock;
+    final response =
+        await http.get(Uri.parse("$baseUrl/projetos"));
+
+    if (response.statusCode == 200) {
+      return jsonDecode(response.body);
+    }
+
+    throw Exception("Erro ao carregar projetos");
   }
 
   Future<List<dynamic>> getVereadores() async {
-    return vereadoresMock;
+    final response =
+        await http.get(Uri.parse("$baseUrl/vereadores"));
+
+    if (response.statusCode == 200) {
+      return jsonDecode(response.body);
+    }
+
+    throw Exception("Erro ao carregar vereadores");
   }
 
   Future<List<dynamic>> getPartidos() async {
-    return partidosMock;
+    final response =
+        await http.get(Uri.parse("$baseUrl/partidos"));
+
+    if (response.statusCode == 200) {
+      return jsonDecode(response.body);
+    }
+
+    throw Exception("Erro ao carregar partidos");
   }
 }
