408e21758b8f1b35c11600466a7a5badb74bda58
Bruna da Silva Donati <brunadasilvadonati@gmail.com>
2026-07-14 15:29:54 -0300

    config para notificações

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,10 +1,56 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'firebase_options.dart';
 
-import 'paginas/splash_screen.dart';
+Future<void> configurarNotificacoes() async {
+  FirebaseMessaging messaging = FirebaseMessaging.instance;
 
-void main() {
+  NotificationSettings settings =
+      await messaging.requestPermission();
+
+  if (settings.authorizationStatus ==
+      AuthorizationStatus.authorized) {
+    print("Permissão concedida");
+
+    String? token = await messaging.getToken();
+    print("TOKEN: $token");
+
+    FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+      print("Título: ${message.notification?.title}");
+      print("Mensagem: ${message.notification?.body}");
+    });
+
+  } else {
+    print("Permissão negada");
+  }
+}
+
+Future<void> firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  print("Mensagem em background: ${message.notification?.title}");
+}
+
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  await configurarNotificacoes();
+
+  FirebaseMessaging.onBackgroundMessage(
+    firebaseMessagingBackgroundHandler,
+  );
+
   runApp(const MyApp());
 }
+
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -13,7 +59,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: SplashScreen(),
+      home: MyApp(),
     );
   }
 }
